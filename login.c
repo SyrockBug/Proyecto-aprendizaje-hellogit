541e1286108697c0902833cc0fc05b2d8310dcd7
Nico <nicomefe@gmail.com>
2024-10-19 14:30:59 +0200

    Version final login

--- login.c
+++ login.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-char nombre[25];
+char nombre[25], cont[15];
 main (void){
 	printf("Introduzca su nombre:");
 	scanf("%s", nombre);
+	printf("Introduzca su contraseña:");
+	scanf("%s", cont);
+	printf("Bienvenido!");
 }
